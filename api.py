ea8d67c3c8da172dfb08937575c3c45e07c2ae87
nafrinmeir <45077386+nafrinmeir@users.noreply.github.com>
2024-11-22 22:06:38 +0200

    Update api.py

--- api.py
+++ api.py
@@ -54,4 +54,4 @@ def get_tasks():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)  # API runs on port 5001
+    app.run(debug=True, host="0.0.0.0", port=5001)  # API runs on port 5001
